27dba4282d4731f334648f293207cb4274f43d26
Jean Paul Rojas <ingjeanpaulrojas@gmail.com>
2026-01-29 15:41:02 -0300

    feat(babel): enhance Babel configuration and add Nativewind support

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function ChatScreen() {
   return (
-    <View>
-      <Text>ChatScreen</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
     </View>
   );
 }
