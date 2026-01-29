2ad066272584a1f4d83b6cbd07f1aea3fe79a9ef
Jean Paul Rojas <ingjeanpaulrojas@gmail.com>
2026-01-29 15:48:06 -0300

    feat(app): remove App component and integrate Poppins font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,22 @@ import '../../global.css';
 
 import { Stack } from 'expo-router';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return <Stack screenOptions={{ headerShown: false }} />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const { colors } = require('./src/config/colors');
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      fontFamily: {
+        'poppins-regular': ['Poppins_400Regular'],
+        'poppins-medium': ['Poppins_500Medium'],
+        'poppins-bold': ['Poppins_700Bold'],
+      },
+    },
   },
   plugins: [],
 };
